bb26de59a94b4a9267a9da8bacec5972c907462a
Amber <amber.jones@usu.edu>
2020-11-29 15:48:25 -0700

    adding exploratory visualizations
    se enter the commit message for your changes. Lines starting

--- Visualization.py
+++ Visualization.py
@@ -1,5 +1,8 @@
+
+# Import Libraries
 #####################
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.sankey import Sankey
 import squarify
@@ -7,19 +10,28 @@ import seaborn as sns
 from pywaffle import Waffle
 #####################
 
-df_events = pd.read_csv('Classified_Events.csv',
-                      engine='python',
-                      header=0,
-                      parse_dates=True,
-                      infer_datetime_format=True)
-
+# Import Classified/Labeled Event Data
+#####################
+df_events = pd.read_csv('Classified_Events.csv', engine='python', header=0, parse_dates=True, infer_datetime_format=True)
 
 #####################
-# Overall Treemap
+# Treemaps
 #####################
+# Group data by label
 df = df_events.groupby('Label', as_index=False)['Volume(gal)'].sum()
 df['Volume(gal)'] = df['Volume(gal)'].astype(int)
+# Reorder rows
+df = df.reindex([3, 2, 5, 4, 0, 1])
+# Concatonate label + volume for plotting
 df['label+vol'] = df['Label'] + '\n(' + df['Volume(gal)'].astype(str) + ' gallons)'
+# Add columns for daily averages
+df['daily'] = (df['Volume(gal)']/14).astype(int)
+df['daily_label+vol'] = df['Label'] + '\n(' + df['daily'].astype(str) + ' gallons)'
+# Subset for indoor water use
+df_indoor = df[(df.Label != 'irrigation') & (df.Label != 'hose')]
+
+# Overall Treemap
+#####################
 labels = df['label+vol']
 sizes = df['Volume(gal)']
 colors = [plt.cm.Spectral(i/float(len(labels))) for i in range(len(labels))]
@@ -30,9 +42,20 @@ plt.title('Overall Water Use')
 plt.axis('off')
 plt.show()
 
+# Daily Average Treemap
+#####################
+labels = df['daily_label+vol']
+sizes = df['daily']
+colors = [plt.cm.Spectral(i/float(len(labels))) for i in range(len(labels))]
+
+plt.figure(figsize=(12, 8), dpi=80)
+squarify.plot(sizes=sizes, label=labels, color=colors, alpha=.8)
+plt.title('Average Daily Water Use')
+plt.axis('off')
+plt.show()
+
 # Indoor Treemap
 #####################
-df_indoor = df[(df.Label != 'irrigation') & (df.Label != 'hose')]
 labels = df_indoor['label+vol']
 sizes = df_indoor['Volume(gal)']
 colors = [plt.cm.Spectral(i/float(len(labels))) for i in range(len(labels))]
@@ -43,29 +66,64 @@ plt.title('Indoor Water Use')
 plt.axis('off')
 plt.show()
 
+# Daily Average Indoor Treemap
 #####################
-# Overall Sankey Plot
+labels = df_indoor['daily_label+vol']
+sizes = df_indoor['daily']
+colors = [plt.cm.Spectral(i/float(len(labels))) for i in range(len(labels))]
+
+plt.figure(figsize=(12, 8), dpi=80)
+squarify.plot(sizes=sizes, label=labels, color=colors, alpha=.8)
+plt.title('Average Daily Indoor Water Use')
+plt.axis('off')
+plt.show()
+
+
 #####################
+# Sankey Plots
+#####################
+# Organize data as in/out flows
 df_flows = df_events.groupby('Label', as_index=False)['Volume(gal)'].sum().sort_values('Volume(gal)', ascending=False)
 df_flows['Volume(gal)'] = -df_flows['Volume(gal)'].astype(int)
 df_flows = df_flows.append(-df_flows.sum(numeric_only=True), ignore_index=True)
 df_flows.iloc[6, 0] = 'Total Volume'
+# Daily averages
+df_flows['daily'] = (df_flows['Volume(gal)']/14).astype(int)
+
+# Overall Plots
+#####################
 flows = df_flows['Volume(gal)']
 labels = df_flows['Label']
 Sankey(head_angle=150, unit='gal', scale=1/24524, offset=0.2,
        flows=flows, labels=labels,
        orientations=[0, -1, 1, -1, -1, -1, 0],
        pathlengths=[0.6, 0.25, 0.25, 0.25, 0.25, 0.25, 0.6]).finish()
-plt.title('Flow diagram')
+plt.title('Flows')
 plt.show()
 
-# Indoor Sankey Plot
+# Daily Sankey Plots
+#####################
+flows = df_flows['daily']
+labels = df_flows['Label']
+Sankey(head_angle=150, unit='gal', scale=1/1751, offset=0.2,
+       flows=flows, labels=labels,
+       orientations=[0, -1, 1, -1, -1, -1, 0],
+       pathlengths=[0.6, 0.25, 0.25, 0.25, 0.25, 0.25, 0.6]).finish()
+plt.title('Daily flows')
+plt.show()
+
+# Indoor Sankey Plots
 #####################
 df_flows = df_events.groupby('Label', as_index=False)['Volume(gal)'].sum().sort_values('Volume(gal)', ascending=False)
 df_indoor_flows = df_flows[(df_flows.Label != 'irrigation') & (df_flows.Label != 'hose')]
 df_indoor_flows['Volume(gal)'] = -df_indoor_flows['Volume(gal)'].astype(int)
 df_indoor_flows = df_indoor_flows.append(-df_indoor_flows.sum(numeric_only=True), ignore_index=True)
-df_indoor_flows.iloc[4, 0] = 'Total Volume'
+df_indoor_flows.iloc[4, 0] = 'Total \n Volume'
+# Daily averages
+df_indoor_flows['daily'] = (df_indoor_flows['Volume(gal)']/14).astype(int)
+
+# Indoor Plot
+#####################
 flows = df_indoor_flows['Volume(gal)']
 labels = df_indoor_flows['Label']
 Sankey(head_angle=150, unit='gal', scale=1/1839, offset=0.2,
@@ -74,6 +132,35 @@ Sankey(head_angle=150, unit='gal', scale=1/1839, offset=0.2,
        pathlengths=[0.6, 0.25, 0.25, 0.25, 0.6]).finish()
 plt.title('Flow diagram')
 plt.show()
+
+# Daily Indoor Plot
+#####################
+flows = df_indoor_flows['daily']
+labels = df_indoor_flows['Label']
+Sankey(head_angle=150, unit='gal', scale=1/131, offset=0.2,
+       flows=flows, labels=labels,
+       orientations=[0, -1, 1, -1, 0],
+       pathlengths=[0.6, 0.25, 0.25, 0.25, 0.6],
+       #color='b'
+       ).finish()
+plt.title('Daily flows')
+plt.show()
+
+fig = plt.figure(figsize=(16, 6), dpi=80)
+ax = fig.add_subplot(1, 1, 1, xticks=[], yticks=[],
+                     title='Daily flows')
+sankey = Sankey(ax=ax, scale=1/131, head_angle=120, unit='gal', shoulder=0.075, margin=0.2, offset=0.15)
+sankey.add(flows=flows, labels=labels, orientations=[0, -1, 1, -1, 0],
+           pathlengths=[0.25, 0.25, 0.25, 0.25, 1],
+           fill = False)  # Arguments to matplotlib.patches.PathPatch
+diagrams = sankey.finish()
+diagrams[0].texts[-1].set_color('r')
+diagrams[0].text.set_fontweight('bold')
+
+
+
+
+
 
 #####################
 # Marginal Histogram
@@ -155,12 +242,138 @@ plt.show()
 
 
 #####################
+# Hourly Aggregation
+#####################
+
+df_hourly = df_events[['Volume(gal)', 'Duration(min)', 'Label']].copy()
+df_hourly['Hour'] = pd.to_datetime(df_events['StartTime']).dt.hour
+df_grouped = df_hourly.groupby(['Label', 'Hour'], as_index=False)['Volume(gal)'].sum()
+df_grouped = df_hourly.groupby(['Label', 'Hour'], as_index=False).aggregate({'Volume(gal)': np.sum, 'Duration(min)': np.average})
+df_grouped = df_hourly.groupby(['Label', 'Hour'], as_index=False).agg({'Volume(gal)': np.sum, 'Duration(min)': ['mean', 'min', 'max']})
+df_grouped.columns = ['Label', 'Hour', 'Volume_tot', 'Duration_mean', 'Duration_min', 'Duration_max']
+
+# Volume
+df_pvt = pd.pivot_table(df_grouped, values='Volume_tot', index=['Hour'], columns=['Label'], fill_value=0)
+df_pvt = df_pvt[['irrigation', 'hose', 'shower', 'toilet', 'clothwasher', 'faucet']]
+
+Labels = list(df_pvt.columns)
+colors = [plt.cm.Spectral(i/float(len(Labels)-1)) for i in range(len(Labels))]
+N = len(df_pvt)
+bottom = np.zeros(N)
+width = 0.5
+for elem, color in zip(Labels, colors):
+    plt.bar(df_pvt.index, df_pvt[elem], bottom=bottom, color=color, width=width)
+    bottom += df_pvt[elem]
+
+plt.ylabel('Volume (gal)')
+plt.xlabel('Hour of Day')
+plt.title('Total Hourly Water Use')
+plt.xticks(df_pvt.index)
+plt.legend(Labels)
+plt.show()
+
+# Without Irrigation
+df_drop = df_pvt.drop(columns=['irrigation', 'hose'], index=[0, 3, 4, 5, 6])
+Labels = list(df_drop.columns)
+colors = [plt.cm.Spectral(i/float(len(Labels)-1)) for i in range(len(Labels))]
+N = len(df_drop)
+bottom = np.zeros(N)
+width = 0.5
+for elem, color in zip(Labels, colors):
+    plt.bar(df_drop.index, df_drop[elem], bottom=bottom, color=color, width=width)
+    bottom += df_drop[elem]
+
+plt.ylabel('Volume (gal)')
+plt.xlabel('Hour of Day')
+plt.title('Total Hourly Water Use')
+plt.xticks(df_drop.index)
+plt.legend(Labels)
+plt.show()
+
+#Side by Side
+bottom = np.zeros(N)
+wide = 0
+for elem, color in zip(Labels, colors):
+    plt.bar(df_drop.index + wide, df_drop[elem], bottom=bottom, color=color, width=0.15)
+    wide += 0.15
+
+plt.ylabel('Volume (gal)')
+plt.xlabel('Hour of Day')
+plt.title('Total Hourly Water Use')
+plt.xticks(df_drop.index)
+plt.legend(Labels)
+plt.show()
+
+#Only Shower
+plt.bar(df_drop.index, df_drop['shower'], width=1, edgecolor='white', color='#2d7f5e')
+plt.ylabel('Volume (gal)')
+plt.xlabel('Hour of Day')
+plt.title('Hourly Shower Volume')
+plt.xticks(df_drop.index)
+plt.show()
+
+#Shower Duration
+df_pvt_duration = pd.pivot_table(df_grouped, values='Duration_mean', index=['Hour'], columns=['Label'], fill_value=0)
+df_pvt_duration = df_pvt_duration[['irrigation', 'hose', 'shower', 'toilet', 'clothwasher', 'faucet']]
+df_drop_duration = df_pvt_duration.drop(columns=['irrigation', 'hose'], index=[0, 3, 4, 5, 6])
+
+df_pvt_duration = pd.pivot_table(df_grouped, values='Duration_max', index=['Hour'], columns=['Label'], fill_value=0)
+df_pvt_duration = df_pvt_duration[['irrigation', 'hose', 'shower', 'toilet', 'clothwasher', 'faucet']]
+df_drop_duration = df_pvt_duration.drop(columns=['irrigation', 'hose'], index=[0, 3, 4, 5, 6])
+
+plt.bar(df_drop_duration.index, df_drop_duration['shower'], width=1, edgecolor='white', color='#2d7f5e')
+plt.ylabel('Duration (min)')
+plt.xlabel('Hour of Day')
+plt.title('Hourly Shower Duration')
+plt.xticks(df_drop.index)
+plt.show()
+
+
+# use box plots? lines of ranges? violin plots?
+
+#####################
 # Grouped Violin Plot
 #####################
-sns.violinplot(x="Label", y="Duration(min)", data=df_events, palette="Pastel1")
-sns.violinplot(x="Label", y="Volume(gal)", data=df_events, palette="Pastel1")
+df_indoor_events = df_events[(df_events.Label != 'irrigation') & (df_events.Label != 'hose')]
+
+sns.violinplot(x="Label", y="Volume(gal)", data=df_indoor_events, palette="Pastel1")
+sns.violinplot(x="Label", y="Duration(min)", data=df_indoor_events, palette="Pastel1")
 sns.plt.show()
 
+# Shower Hourly Violin Plot
+df_hourly_shower = df_hourly[(df_events.Label == 'shower')]
+sns.violinplot(x="Hour", y="Duration(min)", data=df_hourly_shower, palette="Pastel1", cut =0)
+sns.boxplot(x="Hour", y="Duration(min)", data=df_hourly_shower, palette="Pastel1")
+
+
+#####################
+# $23.25 for 10,000 gallons of water.
+# 75 cents per 1,000 gallons if residents use anywhere from 10,001 to 50,000 gallons.
+# 1.50 per 1,000 gallons for all usage over 50,000 gallons
+
+df['monthly'] = df['daily'] * 30
+
+
+# 27154 gallons/acre 0.28*2/3 acres = 5000 gal is 1 inch of water
+# 5000 gallons/week = 20000 gallons/month
+
+
+Labels = ['Indoor', 'Outdoor']
+Values1 = [(5160),10000, ]
+Values2 = [(48600), 40000,]
+Values3 =
+colors = [plt.cm.Spectral(i/float(len(Labels)-1)) for i in range(len(Labels))]
+N = len(df['Label'])
+width = 0.5
+bottom = 0
+for elem, color in zip(Labels, colors):
+    plt.bar(0, Values, bottom=bottom, color=color, width=width)
+    bottom += Values
+
+plt.ylabel('Volume (gal)')
+plt.title('Total Monthly Water Use')
+plt.legend(Labels)
+plt.show()
 
 
 
@@ -168,9 +381,7 @@ sns.plt.show()
 
 
 
-
-
-
+#####################
 
 
 # Overall Waffle Plot
